49afc3c242eea4be7a5ca8dc2d6ce96ca60497a1
dzang <bartlomiej.bielecki36@gmail.com>
2024-10-16 20:33:12 +0200

    lab c

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Repository\ForecastRepository;
+use App\Repository\LocationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{countryCode}/{name}', name: 'app_weather')]
+    public function city(
+        LocationRepository $locationRepository,
+        ForecastRepository $forecastRepository,
+        string $name,
+        string $countryCode,
+    ): Response
+    {
+        $location = $locationRepository->findByCountryCodeAndName($countryCode, $name);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Location not found');
+        }
+
+        $forecasts = $forecastRepository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'forecasts' => $forecasts,
+        ]);
+    }
+}
---
+++ src/Repository/LocationRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Location;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Location>
+ */
+class LocationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Location::class);
+    }
+
+    public function findByCountryCodeAndName(string $countryCode,string $name)
+    {
+        $qb = $this->createQueryBuilder('m');
+        $qb->where('m.countryCode = :countryCode')
+            ->andWhere('m.name = :name')
+            ->setParameter('countryCode', $countryCode)
+            ->setParameter('name', $name)
+        ;
+
+        $query = $qb->getQuery();
+        $result = $query->getResult();
+        return $result;
+    }
+}

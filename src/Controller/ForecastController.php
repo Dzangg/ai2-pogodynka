25d38de92819be8267ddaa146ce2c8a331297623
dzang <bartlomiej.bielecki36@gmail.com>
2024-10-20 19:31:15 +0200

    lab e

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -10,10 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/forecast')]
 final class ForecastController extends AbstractController{
     #[Route(name: 'app_forecast_index', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_INDEX')]
     public function index(ForecastRepository $forecastRepository): Response
     {
         return $this->render('forecast/index.html.twig', [
@@ -22,6 +24,7 @@ final class ForecastController extends AbstractController{
     }
 
     #[Route('/new', name: 'app_forecast_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $forecast = new Forecast();
@@ -44,6 +47,7 @@ final class ForecastController extends AbstractController{
     }
 
     #[Route('/{id}', name: 'app_forecast_show', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_SHOW')]
     public function show(Forecast $forecast): Response
     {
         return $this->render('forecast/show.html.twig', [
@@ -52,6 +56,7 @@ final class ForecastController extends AbstractController{
     }
 
     #[Route('/{id}/edit', name: 'app_forecast_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_EDIT')]
     public function edit(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ForecastType::class, $forecast, [
@@ -72,6 +77,7 @@ final class ForecastController extends AbstractController{
     }
 
     #[Route('/{id}', name: 'app_forecast_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_FORECAST_DELETE')]
     public function delete(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$forecast->getId(), $request->getPayload()->getString('_token'))) {
